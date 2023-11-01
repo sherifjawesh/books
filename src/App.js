9166e22f87b6b33809014752a06c77dabb1ac7c0
Sherif <sherifjawesh@gmail.com.com>
2023-11-01 08:25:51 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Create from "./components/Create";
+import BookList from '/components/List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [books,setBooks] = useState([]);
+
+    const createBook = (title) => {
+        
+        const updatedBooks = [
+            ...books,
+            {id:Math.random(Math.random()*9999),title}
+        ] 
+        setBooks(updatedBooks);
+    };
+    return (
+        <div>
+            <BookList books={books}/>
+            <Create onCreate={createBook}/>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/Show.js
@@ -0,0 +1,5 @@
+function Show({book}) {
+    return <div className="book-show">{book.title}</div>;
+}
+
+export default Show
